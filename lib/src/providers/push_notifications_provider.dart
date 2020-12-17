69669c59b2084839632fd33f60715c6aecc25e9b
Marcelo Rios <chelo38@gmail.com>
2020-12-16 22:22:05 -0400

    Configuracion de los metodos onMessage, onLaunch y onResume

--- lib/src/providers/push_notifications_provider.dart
+++ lib/src/providers/push_notifications_provider.dart
@@ -6,8 +6,23 @@ class PushNotificationProvider {
     _firebaseMessaging.requestNotificationPermissions();
     _firebaseMessaging.getToken().then((token) {
       print('==== FCM TOKEN ====');
-      print(
-          token); //dzp2ZuN3Snq8vLv628A3oV:APA91bHh0IeifDkhXpNDnJ1aeia_5u4qkES0kvtJ5xupJHZ4cykdVgbhY48X3cjt_IVUQHoByGloQ5EupngoCRTHma9MBXzYboa1zvEkbF7z9xSVDtszpFOxy9pTAa3EdsT9LOirFUZB
+      print(token);
+      //dzp2ZuN3Snq8vLv628A3oV:APA91bHh0IeifDkhXpNDnJ1aeia_5u4qkES0kvtJ5xupJHZ4cykdVgbhY48X3cjt_IVUQHoByGloQ5EupngoCRTHma9MBXzYboa1zvEkbF7z9xSVDtszpFOxy9pTAa3EdsT9LOirFUZB
     });
+
+    _firebaseMessaging.configure(
+      onMessage: (info) {
+        print('==== onMessage ====');
+        print(info);
+      }, //Fires when the app is open
+      onLaunch: (info) {
+        print('==== onLaunch ====');
+        print(info);
+      }, //Fires when the app is open
+      onResume: (info) {
+        print('==== onResume ====');
+        print(info);
+      }, //Fires when the app is open
+    );
   }
 }
